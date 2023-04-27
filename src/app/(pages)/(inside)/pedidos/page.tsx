43827eba88befc1bcd6931e1b8ac1f88502373e0
Alan Junqueira <contato.alanjunqueira@gmail.com>
2023-04-26 23:06:17 -0300

    feat: create skelletons on pedidos page

--- src/app/(pages)/(inside)/pedidos/page.tsx
+++ src/app/(pages)/(inside)/pedidos/page.tsx
@@ -4,7 +4,10 @@ import { Refresh, Search } from '@mui/icons-material'
 import {
   Box,
   Button,
+  CircularProgress,
+  Grid,
   InputAdornment,
+  Skeleton,
   TextField,
   Typography,
 } from '@mui/material'
@@ -12,6 +15,7 @@ import { useState } from 'react'
 
 export default function PedidosPage() {
   const [searchInput, setSearchInput] = useState('')
+  const [isLoading, setIsLoading] = useState(true)
 
   const handleSearchInput = () => { }
 
@@ -19,23 +23,27 @@ export default function PedidosPage() {
 
   return (
     <Box sx={{ my: 3 }}>
-      <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
+      <Box sx={{ mb: 3, display: 'flex', justifyContent: 'space-between' }}>
         <Box sx={{ display: 'flex', alignItens: 'center' }}>
           <Typography component="h5" variant="h5" sx={{ color: '#555', mr: 2 }}>
             Pedidos
           </Typography>
-          <Button
-            sx={{ justifyContent: { xs: 'flex-start', md: 'center' } }}
-            size="small"
-          >
-            <Refresh />
-            <Typography
-              component="div"
-              sx={{ color: '#555', display: { xs: 'none', sm: 'block' } }}
+          {isLoading ? (
+            <CircularProgress size={24} />
+          ) : (
+            <Button
+              sx={{ justifyContent: { xs: 'flex-start', md: 'center' } }}
+              size="small"
             >
-              Atualizar
-            </Typography>
-          </Button>
+              <Refresh />
+              <Typography
+                component="div"
+                sx={{ color: '#555', display: { xs: 'none', sm: 'block' } }}
+              >
+                Atualizar
+              </Typography>
+            </Button>
+          )}
         </Box>
         <TextField
           value={searchInput}
@@ -43,6 +51,7 @@ export default function PedidosPage() {
           onKeyUp={handleSearchKey}
           placeholder="Pesquise um pedido"
           variant="standard"
+          disabled={isLoading}
           InputProps={{
             endAdornment: (
               <InputAdornment position="end">
@@ -52,6 +61,49 @@ export default function PedidosPage() {
           }}
         />
       </Box>
+
+      <Grid container spacing={3} columns={{ xs: 1, sm: 2, md: 4 }}>
+        {isLoading && (
+          <>
+            <Grid item xs={1}>
+              <Skeleton variant="rectangular" height={220} />
+            </Grid>
+            <Grid item xs={1}>
+              <Skeleton variant="rectangular" height={220} />
+            </Grid>
+            <Grid item xs={1}>
+              <Skeleton variant="rectangular" height={220} />
+            </Grid>
+            <Grid item xs={1}>
+              <Skeleton variant="rectangular" height={220} />
+            </Grid>
+            <Grid item xs={1}>
+              <Skeleton variant="rectangular" height={220} />
+            </Grid>
+            <Grid item xs={1}>
+              <Skeleton variant="rectangular" height={220} />
+            </Grid>
+            <Grid item xs={1}>
+              <Skeleton variant="rectangular" height={220} />
+            </Grid>
+            <Grid item xs={1}>
+              <Skeleton variant="rectangular" height={220} />
+            </Grid>
+            <Grid item xs={1}>
+              <Skeleton variant="rectangular" height={220} />
+            </Grid>
+            <Grid item xs={1}>
+              <Skeleton variant="rectangular" height={220} />
+            </Grid>
+            <Grid item xs={1}>
+              <Skeleton variant="rectangular" height={220} />
+            </Grid>
+            <Grid item xs={1}>
+              <Skeleton variant="rectangular" height={220} />
+            </Grid>
+          </>
+        )}
+      </Grid>
     </Box>
   )
 }
