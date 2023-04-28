3e9881083aab04ffcac16b2f470bc0da00f02ecc
Alan Junqueira <contato.alanjunqueira@gmail.com>
2023-04-28 17:53:55 -0300

    style: Create OrderItem card style

--- src/components/OrderItem.tsx
+++ src/components/OrderItem.tsx
@@ -1,5 +1,6 @@
 import { Order } from '@/@types/Order'
-import { Box, BoxProps } from '@mui/material'
+import { OrderStatus } from '@/@types/OrderStatus'
+import { Box, BoxProps, Button, Typography } from '@mui/material'
 import React from 'react'
 
 interface IOrderItem extends BoxProps {
@@ -7,10 +8,67 @@ interface IOrderItem extends BoxProps {
 }
 
 export const OrderItem = ({ order, ...props }: IOrderItem) => {
-  // const { } = order
+  const {
+    id,
+    orderDate,
+    paymentMethod,
+    products,
+    shippingAddress,
+    shippingPrice,
+    status,
+    subtotal,
+    total,
+    userId,
+    cashAdvanceValue,
+    cupom,
+    cupomDiscount,
+    userName,
+  } = order
+
+  const getStatusBackground = (status: OrderStatus) => {
+    const statuses = {
+      preparing: '#2787BA',
+      sent: '#27BA3A',
+      delivered: '#999999',
+    }
+
+    return statuses[status]
+  }
   return (
     <>
-      <Box {...props}>...</Box>
+      <Box
+        {...props}
+        sx={{
+          border: '1px solid #eee',
+          color: '#FFF',
+          borderRadius: 2,
+          overflow: 'hidden',
+          ...props.sx,
+        }}
+      >
+        <Box
+          sx={{
+            display: 'flex',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+            p: 1,
+            backgroundColor: getStatusBackground(status),
+          }}
+        >
+          <Box>
+            <Typography component="p">{orderDate}</Typography>
+            <Typography component="p">{userName}</Typography>
+            <Button size="small" sx={{ color: '#FFF', p: 0 }}>
+              Imprimir
+            </Button>
+          </Box>
+          <Box>
+            <Typography component="p" sx={{ fontSize: 24 }}>
+              #{id}
+            </Typography>
+          </Box>
+        </Box>
+      </Box>
     </>
   )
 }
